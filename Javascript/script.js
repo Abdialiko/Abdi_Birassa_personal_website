c5c60656dab270503475f36f26c2b4ec660ed10d
Abdialiko <abaliko82@gmail.com>
2024-02-26 16:42:54 +0300

    html,css and javascript update

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,114 @@
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls (if you have dot controls for navigation)
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+// Optional: Add auto slide feature
+let slideInterval = setInterval(function() { plusSlides(1); }, 5000); // Change slides every 5 seconds
+
+// Add event listeners for the next and previous buttons
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+var typed = new Typed(".typing-text", {
+    strings: [ "project managment", "IT Coordinator", "System Analyst",  "ICT Officer"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
+document.querySelector('.prev').addEventListener('click', function() {
+  plusSlides(-1);
+  resetInterval();
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+  plusSlides(1);
+  resetInterval();
+});
+
+// Resets the slide interval when manually navigating
+function resetInterval() {
+  clearInterval(slideInterval);
+  slideInterval = setInterval(function() { plusSlides(1); }, 5000);
+}
+
+const srtop = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1000,
+    reset: true
+});
+
+/* SCROLL HOME */
+srtop.reveal('.home .content h3', { delay: 200 });
+srtop.reveal('.home .content p', { delay: 200 });
+srtop.reveal('.home .content .btn', { delay: 200 });
+
+srtop.reveal('.home .image', { delay: 400 });
+srtop.reveal('.home .linkedin', { interval: 600 });
+srtop.reveal('.home .github', { interval: 800 });
+srtop.reveal('.home .twitter', { interval: 1000 });
+srtop.reveal('.home .telegram', { interval: 600 });
+srtop.reveal('.home .instagram', { interval: 600 });
+srtop.reveal('.home .dev', { interval: 600 });
+
+/* SCROLL ABOUT */
+srtop.reveal('.about .content h3', { delay: 200 });
+srtop.reveal('.about .content .tag', { delay: 200 });
+srtop.reveal('.about .content p', { delay: 200 });
+srtop.reveal('.about .content .box-container', { delay: 200 });
+srtop.reveal('.about .content .resumebtn', { delay: 200 });
+
+
+/* SCROLL SKILLS */
+srtop.reveal('.skills .container', { interval: 200 });
+srtop.reveal('.skills .container .bar', { delay: 400 });
+
+/* SCROLL EDUCATION */
+srtop.reveal('.education .box', { interval: 200 });
+
+/* SCROLL PROJECTS */
+srtop.reveal('.work .box', { interval: 200 });
+
+/* SCROLL EXPERIENCE */
+srtop.reveal('.experience .timeline', { delay: 400 });
+srtop.reveal('.experience .timeline .container', { interval: 400 });
+
+/* SCROLL CONTACT */
+srtop.reveal('.contact .container', { delay: 400 });
+srtop.reveal('.contact .container .form-group', { delay: 400 });
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.nav-toggle').addEventListener('click', function () {
         var nav = document.querySelector('.nav-link ul');
         if (nav.style.display === "block") {
-            nav.style.display = "none";
+            nav.style.display = "none"; 
         } else {
             nav.style.display = "block";
         }
@@ -11,8 +116,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-function showDetails(projectId) {
-    
+function showDetails(projectId = 'erp') {
     var titles = {
         'erp': 'Enterprise resource planning System (ERP) [18/03/2018 – 22/12/2018] ',
         'crime': 'Crime management system [13/02/2019 – 18/06/2019] ',
@@ -24,14 +128,14 @@ function showDetails(projectId) {
         'gotrip': `GoTrip`,
     };
     var Image = {
-        'erp': 'Enterprise resource planning System (ERP) [18/03/2018 – 22/12/2018] ...',
-        'crime': 'Crime management system [13/02/2019 – 18/06/2019] ...',
-        'union': 'Union/Cooperative information management system [05/08/2019 – 27/03/2020] ...',
-        'survey': 'Survey management system (web based and mobile application) [22/04/2020 – 06/11/2020] ...',
-        'guardium': 'IBM Security Guardium Data Protection for Databases [22/12/2020 – 18/03/2021] ...',
-        'waf': 'WAF F5 (Web Application Firewall) [08/04/2021 – 21/06/2021] ...',
-        'book': 'Book and Inventory management System ...',
-        'gotrip': ` <img src="./cer1.jpg" class="cer" alt="Certificate Image">`,
+        'erp': '    <img src="../Image/edu.jpg     " alt="Certificate Image">',
+        'crime': '  <img src="../Image/crime.jpg   "  alt="Certificate Image">',
+        'union': '  <img src="(../Image/coop.jpg   "  alt="Certificate Image">',
+        'survey': ' <img src="../Image/survey.jpg  "  alt="Certificate Image">',
+        'guardium':'<img src="../Image/guardium.jpg"  alt="Certificate Image">',
+        'waf': '    <img src="../Image/waf5.jpg    "  alt="Certificate Image">',
+        'book': '   <img src="../Image/library.jpg" class="pro"  alt="Certificate Image">',
+        'gotrip': ` <img src="./cer1.jpg" class="pro" alt="Certificate Image">`,
     };
     var details = {
         'erp': `'Enterprise resource planning System (ERP) [18/03/2018 – 22/12/2018] This system is developed for Addis Ababa government office. Currently as pilot six governments office have been
@@ -78,6 +182,7 @@ function showDetails(projectId) {
     document.getElementById('project-titles').innerHTML = titles[projectId] || 'No titles available';
     document.getElementById('project-details').innerHTML = details[projectId]  || 'No details available';
     document.getElementById('project-Image').innerHTML = Image[projectId]  || 'No details available';
+    togglePopup();
 }
 function showcer(cerid) {
     var details = {
@@ -87,15 +192,12 @@ function showcer(cerid) {
     };
     var imageUrl = details[cerid] || 'No details available';
     if (cerid === 'cer1') {
-        document.getElementById('cer').innerHTML = `<img src="${imageUrl}" class="cer" alt="Certificate Image">
-        </br>.</br></br>
-        <div class="verify">
-        <a class="btn0" href="https://www.credly.com/badges/78103b3a-2156-4d56-bf44-edcfb5629c23/public_url"> Verify Here</a> </div>`;
+        document.getElementById('cer').innerHTML = `<img src="${imageUrl}" class="cer" alt="Certificate Image">`
+        document.getElementById('cer1').innerHTML = `<div class="verify"><a class="btn0" href="https://www.credly.com/badges/78103b3a-2156-4d56-bf44-edcfb5629c23/public_url"> Verify Here</a> </div>`;
+
     } else if (cerid === 'cer2') {
-        document.getElementById('cer').innerHTML = `<img src="${imageUrl}" class="cer" alt="Certificate Image">
-        </br>.</br></br>
-        <div class="verify">
-        <a class="btn0" href="https://www.credly.com/badges/aafc1378-e0c2-4845-8813-5cfa30374483/public_url"> Verify Here</a> </div>`;
+        document.getElementById('cer').innerHTML = `<img src="${imageUrl}" class="cer" alt="Certificate Image">`
+        document.getElementById('cer1').innerHTML = `<div class="verify"><a class="btn0" href="https://www.credly.com/badges/aafc1378-e0c2-4845-8813-5cfa30374483/public_url"> Verify Here</a> </div>`;
     }else{
         document.getElementById('cer').innerHTML = `<img src="${imageUrl}" class="cer1" alt="Certificate Image">`;
     }
@@ -125,31 +227,10 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('showing');
     });
 });
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  slideIndex += n;
-  showSlides(slideIndex);
+function togglePopup() {
+    var popup = document.querySelector('.popup');
+    var overlay = document.querySelector('.overlay');
+    var isShown = window.getComputedStyle(popup).display === "block";
+    popup.style.display = isShown ? "none" : "block";
+    overlay.style.display = isShown ? "none" : "block";
 }
-
-// Thumbnail image controls - if you decide to add thumbnails
-function currentSlide(n) {
-  slideIndex = n;
-  showSlides(slideIndex);
-}
-
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
-}
-
-
